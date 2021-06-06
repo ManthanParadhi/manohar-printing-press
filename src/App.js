a00269d93cc749d63768046390974b9e2e260c86
ManthanParadhi <66413955+ManthanParadhi@users.noreply.github.com>
2021-06-06 20:09:57 +0530

    run everything nicely

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import UploadForm from './Comps/UploadForm';
 import ImageGrid from './Comps/ImageGrid';
+import { useState } from 'react';
+import { Model } from './Comps/Model';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div>
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && <Model selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
     </div>
   );
 }
---
+++ src/Comps/Model.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { motion, AnimatePresence, animate } from 'framer-motion';
+
+export const Model = ({ selectedImg, setSelectedImg }) => {
+    const deselect = (e) => {
+        if (e.target.classList.contains('backdrop')){
+            setSelectedImg(null);
+        }
+    }
+    return (
+            
+            <motion.div className="backdrop" onClick={deselect} initial={{opacity:0}} animate={{opacity:1}} >
+                
+                    <motion.img src={selectedImg} alt=''
+                    initial={{scale:0}}
+                    animate={{scale:1}}
+                    />
+                
+            </motion.div>
+            
+    )
+}
