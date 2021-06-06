c795182ef5bd1a58cd2d0b64d383a844e485bf26
ManthanParadhi <66413955+ManthanParadhi@users.noreply.github.com>
2021-06-06 17:00:45 +0530

    connected database successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import UploadForm from './Comps/UploadForm';
+import ImageGrid from './Comps/ImageGrid';
 
 function App() {
   return (
     <div>
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
