8adba32700b1dde773f1541dbade237ff6455220
ManthanParadhi <66413955+ManthanParadhi@users.noreply.github.com>
2021-07-04 17:46:21 +0530

    working fine opacity problem solved
    the opacity was compromised during build

--- src/Comps/Navigation.js
+++ src/Comps/Navigation.js
@@ -1,115 +1,12 @@
 import React from 'react';
-// import $ from 'jquery';
 import { Telephone } from '../Logos/Telephone';
 import { Call } from '../Logos/Call';
+import { Link } from 'react-router-dom';
 const Navigation = () => {
     
-    // var animationDelay = 2500,
-	// 	//loading bar effect
-	// 	//letters effect
-	// 	lettersDelay = 50
-
-	// function initHeadline() {
-	// 	//insert <i> element for each letter of a changing word
-	// 	singleLetters($('.cd-headline.letters').find('b'));
-	// 	//initialise headline animation
-	// 	animateHeadline($('.cd-headline'));
-	// }
-
-	// function singleLetters($words) {
-		
-	// 	$words.each(function(){
-	// 		var word = $(this),
-	// 			letters = word.text().split(''),
-	// 			selected = word.hasClass('is-visible');
-	// 			console.log(typeof(letters))
-	// 		for (var i in letters) {
-	// 			if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
-	// 			letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
-	// 		}
-	// 	    var newLetters = letters.join('');
-	// 	    word.html(newLetters).css('opacity', 1);
-	// 	});
-	// }
-
-	// function animateHeadline($headlines) {
-	// 	var duration = animationDelay;
-	// 	$headlines.each(function(){
-	// 		var headline = $(this);
-			
-	// 		//trigger animation
-	// 		setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
-	// 	});
-	// }
-
-	// function hideWord($word) {
-	// 	var nextWord = takeNext($word);
-		
-	// 	if($word.parents('.cd-headline').hasClass('letters')) {
-	// 		var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
-	// 		hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
-	// 		showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
-
-	// 	} 
-	// }
-
-	
-	// function hideLetter($letter, $word, $bool, $duration) {
-	// 	$letter.removeClass('in').addClass('out');
-		
-	// 	if(!$letter.is(':last-child')) {
-	// 	 	setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);  
-	// 	} else if($bool) { 
-	// 	 	setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
-	// 	}
-
-	// 	if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
-	// 		var nextWord = takeNext($word);
-	// 		switchWord($word, nextWord);
-	// 	} 
-	// }
-
-	// function showLetter($letter, $word, $bool, $duration) {
-	// 	$letter.addClass('in').removeClass('out');
-		
-	// 	if(!$letter.is(':last-child')) { 
-	// 		setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration); 
-	// 	} else { 
-	// 		if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
-	// 		if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
-	// 	}
-	// }
-
-	// function takeNext($word) {
-	// 	return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
-	// }
-
-	
-
-	// function switchWord($oldWord, $newWord) {
-	// 	$oldWord.removeClass('is-visible').addClass('is-hidden');
-	// 	$newWord.removeClass('is-hidden').addClass('is-visible');
-	// }
-
-    // useEffect(() => {
-    //     initHeadline();
-        
-    // });
     return (
         <div>    
             <nav className="dark text-center">
-                {/* <section className="cd-intro">
-                    <h1 className="cd-headline letters rotate-3">
-                        <span>Manohar{" "}</span>
-                        <span className="cd-words-wrapper">
-                            <b className="is-visible">Printing</b>
-                            <b>Press</b>
-							<b>Photo</b>
-							<b>Studio</b>
-                        </span>
-                    </h1>
-                </section> */}
-
 				<h1> Manohar Printing Press and Photo Studio</h1>
 				<div>
 				<Telephone/>
@@ -117,7 +14,7 @@ const Navigation = () => {
 				</div>
 				<div>
 				<Call />
-				<h3>9421936685 / 9421983063</h3>
+				<Link to="tel:+919421936685"> HI </Link><h3>9421936685 / 9421983063</h3>
 				</div>
             </nav>
         </div>
